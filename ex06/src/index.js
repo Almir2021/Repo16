3df9bf5d9ff7cfd4bb29aa54348064a3833dce1b
Almir2021 <bosnjicalmir@gmail.com>
2021-09-29 10:10:17 +0200

    vjezba 6

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+   const firstName = function(firstName)  {
+     var fun1  =  "firstName".toUpperCase;
+       return fun1;
+   }
+
+   const lastName = function(lastName){
+       var fun2 = "lastName".toLowerCase;
+
+       return fun2;
+   }
+
+
+   console.log(firstName("almir"));
+   console.log (lastName("BOSNJIC"));
+
+   module.exporst = firstName;
+  module.exporst = lastName;
